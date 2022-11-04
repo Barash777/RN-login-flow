e4a5d1db06ede4541e9005a1fac66302419f4f80
barash777 <barash.vertihvost@gmail.com>
2022-11-04 14:17:47 +0300

    refactor: delete commented properties

--- src/styles/main.ts
+++ src/styles/main.ts
@@ -13,29 +13,22 @@ export const styles = StyleSheet.create({
     stackScreen: {
         flex: 1,
         backgroundColor: '#fff',
-        // alignItems: 'center',
         justifyContent: 'center',
     },
     input: {
         height: 40,
         marginHorizontal: 25,
         marginVertical: 10,
-        // width: "100%",
         borderColor: "gray",
         borderWidth: 1,
         borderRadius: 10,
         padding: 10,
     },
     buttonsInRow: {
-        // flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        // alignItems: "center",
-        // flexGrow: 1
-        // flexWrap: 'nowrap'
     },
     button: {
-        // flex: 1,
         backgroundColor: buttonBGColor,
         alignItems: "center",
         padding: 10,
